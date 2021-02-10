653bc59f39141a6caa440b78b41187319b40dac7
Mateus Med <mateus.medeiros46@gmail.com>
2021-02-10 13:20:39 -0300

    commit to save

--- src/IO/pdf/pdf.js
+++ src/IO/pdf/pdf.js
@@ -39,32 +39,33 @@ function htmlText(){
             <div class="container">
                 <div class="row">
                     <div class="col-xs-6">
-                        <div class="card" style="width: 15rem;">
-                            <img class="card-img-top" src="#" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
+                        <div class="card" style="width: 18rem;">
+                                  <img class="card-img-top" src="..." alt="Card image cap">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
                     </div>
                     <div class="col-xs-6">
             
-                        <div class="card" style="width: 15rem;">
-                            <img class="card-img-top" src="#" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
+                        <div class="card" style="width: 18rem;">
+                                  <img class="card-img-top" src="..." alt="Card image cap">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
                     </div>
                     <div class="col-xs-6">
-                        <div class="card" style="width: 15rem;">
-                            <img class="card-img-top" src="#" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
+                        <div class="card" style="width: 18rem;">
+                                  <img class="card-img-top" src="..." alt="Card image cap">
+                                  <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
                     </div>
                 </div>
             </div>
@@ -86,6 +87,9 @@ function htmlText(){
 
 
         </html>
+
+</body>
+</html>
            
         `
 }
